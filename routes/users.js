a19705ce7c273219c81e0c018f636dfa0d91b229
Ma Yanbin <myanbin@gmail.com>
2020-01-07 20:12:49 +0800

    增加文件上传功能

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+
+var upload = multer({ dest: path.join('uploads') });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/upload', upload.single('avatar'), function (req, res, next) {
+  const file = req.file;
+  fs.copyFile(file.path, path.join('uploads', file.originalname), (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send('success');
+  });
 });
 
 module.exports = router;
